1f4be667aa78537ef59a57a73becfd4c97034560
Reid Vandewiele <reid@puppetlabs.com>
2017-07-26 11:41:15 -0700

    Add explanatory text and comments

--- lib/puppet/indirector/node/mux.rb
+++ lib/puppet/indirector/node/mux.rb
@@ -4,9 +4,23 @@ require 'hocon/config_factory'
 require 'deep_merge'
 
 class Puppet::Node::Mux < Puppet::Indirector::Plain
-  desc "Multiplexes node information from multiple other node terminii."
+  desc "Multiplexes node information from multiple other node terminii.
+    Configured using a file called $confdir/mux.conf. Format is HOCON.
+    Should have a single key, 'terminii', set to a list of values. Each
+    value should be the name of a valid node terminus. Example:
 
-  # Just return an empty node.
+    file:/etc/puppetlabs/puppet/mux.conf
+    ------------------------------------
+    terminii: [
+      exec,
+      console,
+    ]
+
+    The first terminus listed will determine a node's environment.
+    Parameters and classes may be supplied by any terminus, with all such
+    values being merged together. In the event of a conflict, terminii
+    listed earlier in the list take precedence over those listed later."
+
   def find(request)
     # Create a variable to hold the node object to be built
     node = nil
@@ -25,9 +39,9 @@ class Puppet::Node::Mux < Puppet::Indirector::Plain
           Puppet::Indirector::Indirection.instance(:node).terminus_class = terminus_name.to_sym
           found_node = Puppet::Indirector::Indirection.instance(:node).find(request.key, request.options)
 
-          # Note the node's environment, if one was assigned. The environment
-          # assigned by the highest-priority node terminus will be used for the
-          # final node object's environment value.
+          # Note the node's environment, if one is present. The environment
+          # supplied by the highest-priority node terminus (first listed in the
+          # config) will be used for the final node object's environment value.
           environment = found_node.environment if found_node.environment
 
           memo.deep_merge(found_node.to_data_hash)
